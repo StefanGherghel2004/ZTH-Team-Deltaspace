bb8b815cda2ba8926154930d8db625b359b788fb
stun14 <mihaiviorelstan@gmail.com>
2026-07-03 18:34:20 +0300

    Removed showFeed and added getRandomizedFeed method

--- App/CLI-backend/src/main/java/cli/backend/handlers/PostHandler.java
+++ App/CLI-backend/src/main/java/cli/backend/handlers/PostHandler.java
@@ -7,11 +7,8 @@ import java.util.*;
 
 public class PostHandler {
 
-    private User user;
     private List<Post> posts;
     private static PostHandler instance;
-
-    Scanner scan = new Scanner(System.in);
 
     private PostHandler () {
 
@@ -41,26 +38,6 @@ public class PostHandler {
     public void addPost (User user, String postTitle, String postContents, String imageLink) {
 
         posts.add(new Post(user,postTitle,postContents,imageLink));
-    }
-
-    public void showFeed (List<Post> posts) {
-
-        List<Post> currentFeed = getRandomizedFeed(posts);
-
-        int currentPost = 0;
-
-        String choice = null;
-
-        while (!choice.equals("4")) {
-
-            showPost(currentFeed.get(currentPost));
-            if (currentPost > 0) {
-                System.out.println("1. Previous post | 2. See comments | 3. Next post | 4. Back");
-            }
-            else{
-                System.out.println("1. See comments | 2. Next post | 3. Back");
-            }
-        }
     }
 
     public List<Post> getRandomizedFeed(List<Post> posts) {
