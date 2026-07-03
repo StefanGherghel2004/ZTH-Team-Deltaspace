e8d101c6e34b74707bd2d52faf29e4668ffd9822
SrbanM <serban2011@yahoo.com>
2026-07-03 13:49:51 +0300

    Clasa Comment provizorie

--- App/CLI-backend/src/main/java/cli/backend/Comment.java
+++ App/CLI-backend/src/main/java/cli/backend/Comment.java
@@ -2,5 +2,28 @@ package cli.backend;
 
 public class Comment {
     private String text;
+    private User username;
+    private Post idPost;
+    private int iDComment;
+    private static int counter=1;
+
+    public Comment(String text, User username, Post Id){
+        this.text=text;
+        this.username=username;
+        this.idPost=Id;
+        this.iDComment =counter++;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public User getUsername(){
+        return username;
+    }
+
+    public int getId(){
+        return iDComment;
+    }
 
 }
