4448e4ddad3efe138d730e2bf6aa4ebe148f78c2
stun14 <mihaiviorelstan@gmail.com>
2026-07-03 16:14:25 +0300

    PostHandler bases

--- App/CLI-backend/src/main/java/cli/backend/handlers/PostHandler.java
+++ App/CLI-backend/src/main/java/cli/backend/handlers/PostHandler.java
@@ -1,4 +1,44 @@
 package cli.backend.handlers;
 
+import cli.backend.Post;
+import cli.backend.User;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class PostHandler {
+
+    private User user;
+    private List<Post> post;
+    private static PostHandler instance;
+
+    Scanner scan = new Scanner(System.in);
+
+    private PostHandler () {
+
+    }
+
+    public static PostHandler getInstance () {
+
+        if (instance == null) {
+            instance = new PostHandler();
+        }
+
+        return instance;
+    }
+
+    public void showPost (Post post) {
+
+        System.out.println("User: " + post.getUser().getUsername());
+        System.out.println("Title: " + post.getPostTitle() + "\n");
+        System.out.println(post.getPostContents());
+
+        if (post.getImageLink() != null) {
+            System.out.println("Image link: " + post.getImageLink());
+        }
+
+        System.out.println("1. Show comments");
+    }
+
+    public void createPost (User user, )
 }
