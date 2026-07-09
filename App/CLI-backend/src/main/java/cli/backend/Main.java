6316e18ed75329957354f4454fc17808fd9ed380
Stefan-Ciprian Gherghel <stefan.gherghel@stud.acs.upb.ro>
2026-07-09 15:58:05 +0300

    Logger

--- App/CLI-backend/src/main/java/cli/backend/Main.java
+++ App/CLI-backend/src/main/java/cli/backend/Main.java
@@ -1,12 +1,14 @@
 package cli.backend;
 
 import cli.backend.handlers.AppHandler;
+import cli.backend.loggers.*;
 
 public class Main {
     public static void main(String[] args) {
 
         AppHandler app = AppHandler.getInstance();
 
+        Logger.init();
         app.run();
     }
 }
