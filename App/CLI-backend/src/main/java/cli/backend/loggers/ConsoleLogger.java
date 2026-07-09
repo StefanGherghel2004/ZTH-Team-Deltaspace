b1b70c4780081aeeaf8bed416787a0d4fa65f19e
Stefan-Ciprian Gherghel <stefan.gherghel@stud.acs.upb.ro>
2026-07-09 14:46:58 +0300

    Almost done with Logger

--- App/CLI-backend/src/main/java/cli/backend/loggers/ConsoleLogger.java
+++ App/CLI-backend/src/main/java/cli/backend/loggers/ConsoleLogger.java
@@ -1,8 +1,19 @@
 package cli.backend.loggers;
 
 public class ConsoleLogger implements Loggable {
+
+    private LogLevel level;
+
+    public ConsoleLogger(LogLevel logLevel) {
+        this.level = logLevel;
+    }
+
     @Override
     public void log(LogLevel level, String message) {
+
+        if (this.level != level) {
+            return;
+        }
         System.out.println(message);
     }
 }
--- App/CLI-backend/src/main/java/cli/backend/loggers/FileLogger.java
+++ App/CLI-backend/src/main/java/cli/backend/loggers/FileLogger.java
@@ -1,15 +1,16 @@
 package cli.backend.loggers;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileLogger implements Loggable {
-    FileWriter writer;
+    BufferedWriter writer;
     private LogLevel level;
 
     public FileLogger(LogLevel logLevel, String filename) {
         try {
-            writer = new FileWriter(filename);
+            writer = new BufferedWriter(new FileWriter(filename));
         } catch (IOException e) {
             System.out.println("Error opening file: " + filename);
         }
@@ -20,6 +21,11 @@ public class FileLogger implements Loggable {
 
     @Override
     public void log(LogLevel level, String message) {
+
+        if (this.level != level) {
+            return;
+        }
+        
         try {
             writer.write(message + "\n");
             writer.flush();
