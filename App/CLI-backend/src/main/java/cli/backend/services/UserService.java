0f8ab5e0f343d6801799464309758eefe357b41f
stun14 <mihaiviorelstan@gmail.com>
2026-07-13 18:59:30 +0300

    deleted Book1.xlsx file

--- App/CLI-backend/src/main/java/cli/backend/services/UserService.java
+++ App/CLI-backend/src/main/java/cli/backend/services/UserService.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
