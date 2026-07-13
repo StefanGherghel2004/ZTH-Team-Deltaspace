d0a120206b52f7c6275980098192c708972c7f34
SrbanM <serban2011@yahoo.com>
2026-07-13 11:57:13 +0300

    Excel Read

--- App/CLI-backend/src/main/java/cli/backend/Main.java
+++ App/CLI-backend/src/main/java/cli/backend/Main.java
@@ -1,6 +1,6 @@
 package cli.backend;
 
-import cli.backend.database.ExcelRead;
+
 import cli.backend.handlers.AppHandler;
 import cli.backend.loggers.*;
 
@@ -8,9 +8,8 @@ public class Main {
     public static void main(String[] args) {
 
         AppHandler app = AppHandler.getInstance();
-        ExcelRead excelRead=new ExcelRead("App/CLI-backend/databases/Book1.xlsx");
-        excelRead.readExcel();
-        //Logger.init();
-        //app.run();
+
+        Logger.init();
+        app.run();
     }
 }
