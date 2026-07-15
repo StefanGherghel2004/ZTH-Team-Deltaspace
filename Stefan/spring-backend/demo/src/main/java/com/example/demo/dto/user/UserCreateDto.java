1c723b634a3259fc0b3a65fa8323f4feaa756ab2
Stefan-Ciprian Gherghel <stefan.gherghel@stud.acs.upb.ro>
2026-07-15 15:15:08 +0300

    Regex validation in Dto for createUser

--- Stefan/spring-backend/demo/src/main/java/com/example/demo/dto/user/UserCreateDto.java
+++ Stefan/spring-backend/demo/src/main/java/com/example/demo/dto/user/UserCreateDto.java
@@ -3,18 +3,26 @@ package com.example.demo.dto.user;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.time.LocalDate;
 
 @Data
 public class UserCreateDto {
+
     @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Username can only contain letters and numbers")
     private String username;
 
     @NotBlank(message = "Email is required")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+            message = "Password must be at least 8 characters long, contain an uppercase letter, a digit, and a special character")
     private String password;
 
     @NotNull(message = "Date of birth is required")
