c3c80eb84af4edae8d02a186b29981f01777c9c4
SrbanM <serban2011@yahoo.com>
2026-07-16 16:48:56 +0300

    community created not finished

--- App/spring-backend/demo/src/main/java/com/example/demo/controller/CommunityController.java
+++ App/spring-backend/demo/src/main/java/com/example/demo/controller/CommunityController.java
@@ -2,11 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.community.CommunityCreateDto;
 import com.example.demo.mapper.CommunityMapper;
+import com.example.demo.model.Community;
+import com.example.demo.repository.CommunityRepository;
 import com.example.demo.service.CommunityService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/communities")
@@ -14,10 +18,17 @@ import org.springframework.web.bind.annotation.*;
 public class CommunityController {
     private final CommunityService communityService;
     private final CommunityMapper communityMapper;
+    private final CommunityRepository communityRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CommunityCreateDto addCommunity(@Valid @RequestBody CommunityCreateDto createDto){
-        return communityMapper.toResponseDto(savedCommunity);
+    public Community addCommunity(@Valid @RequestBody CommunityCreateDto createDto){
+
+        return communityService.addCommunity(createDto);
+    }
+
+    @GetMapping
+    public List<Community> listAllCommunities(){
+        return communityService.listAllCommunities();
     }
 }
