dbe27207621ae1ea5ec4690f20f5445dcfeab20e
stun14 <mihaiviorelstan@gmail.com>
2026-07-16 16:29:21 +0300

    Implemented Comment model

---
+++ App/spring-backend/demo/src/main/java/com/example/demo/model/Comment.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "comments")
+@SQLDelete(sql = "UPDATE users SET deleted = true WHERE id=?")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = false,nullable = false)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "comments"})
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id", nullable = true)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "replies", "parentComment"})
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("parentComment")
+    private List<Comment> replies;
+}
