57d4d842b75f1833448f3c5fa40ffbce56a839fa
Stefan-Ciprian Gherghel <stefan.gherghel@stud.acs.upb.ro>
2026-07-20 16:13:23 +0300

    Init ImageProcessor.

---
+++ App/ImageProcessor/Controllers/FilterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageProcessor.Controllers
+{
+    [ApiController]
+    [Route("/api/filter")]
+    public class FilterController : ControllerBase
+    {
+
+        [HttpPost]
+        public IFormFile Filter(IFormFile file)
+        {
+            return file;
+        }
+
+    }
+}
