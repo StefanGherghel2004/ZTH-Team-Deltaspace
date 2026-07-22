a3baf12c7647ad82a7c86f1aa187c077a736bb4f
Stefan-Ciprian Gherghel <stefan.gherghel@stud.acs.upb.ro>
2026-07-22 16:29:58 +0300

    Add clearScreen method in console (not sure if we'll use it)

--- App/CLI-backend/src/main/java/cli/backend/readers/Console.java
+++ App/CLI-backend/src/main/java/cli/backend/readers/Console.java
@@ -64,6 +64,11 @@ public class Console {
         System.out.println(message);
     }
 
+    public void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public void printIndexList(String title, List<String> content) {
         info(title);
         for (String i : content) {
