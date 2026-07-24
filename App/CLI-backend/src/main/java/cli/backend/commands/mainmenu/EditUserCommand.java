0125cc45a50a85c2e383db1e05000f1804719659
MihaiStan <mihaiviorelstan@gmail.com>
2026-07-24 13:56:05 +0300

    Refactored the code to get rid of duplicate lines

--- App/CLI-backend/src/main/java/cli/backend/commands/mainmenu/EditUserCommand.java
+++ App/CLI-backend/src/main/java/cli/backend/commands/mainmenu/EditUserCommand.java
@@ -2,15 +2,11 @@ package cli.backend.commands.mainmenu;
 
 import cli.backend.User;
 import cli.backend.commands.Command;
-import cli.backend.duplicates.CheckDuplicate;
-import cli.backend.duplicates.UserDuplicate;
 import cli.backend.handlers.AppHandler;
 import cli.backend.repositories.UserRepository;
 import cli.backend.services.PasswordService;
-import cli.backend.services.UserService;
 import cli.backend.userinterface.readers.Console;
 
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -25,9 +21,7 @@ public class EditUserCommand implements Command {
     public boolean execute() {
         AppHandler app = AppHandler.getInstance();
         Console console = Console.getInstance();
-        UserService userService = UserService.getInstance();
         UserRepository userRepository = UserRepository.getInstance();
-        CheckDuplicate userCheck = new UserDuplicate();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
 
@@ -35,62 +29,22 @@ public class EditUserCommand implements Command {
 
         switch (editType){
             case "username" -> {
-                String newUsername;
-                while(true) {
-                    newUsername = console.getStringInput("Please enter your new username (4-20 characters, alphanumeric):");
-
-                    if (!userService.validateUsername(newUsername)) {
-                        console.error("Invalid username format. Please try again.");
-                        continue;
-                    }
-
-                    if (userCheck.isDuplicate(newUsername)) {
-                        console.error("Username already exists. Please choose a different username.");
-                        continue;
-                    }
-                    break;
-                }
+                String newUsername = console.getValidUsernameInput();
                 userToEdit.setUsername(newUsername);
             }
 
             case "email" -> {
-                String newEmail;
-                while(true){
-                    newEmail = console.getStringInput("Please enter your email address:");
-                    if(!userService.validateEmail(newEmail)) {
-                        console.error("Invalid email format. Must be like 'user@domain.com'.");
-                        continue;
-                    }
-                    if(userCheck.isDuplicate(newEmail)) {
-                        console.error("Email already exists. Please use a different email address.");
-                        continue;
-                    }
-                    break;
-                }
+                String newEmail = console.getValidEmailInput();
                 userToEdit.setEmail(newEmail);
             }
 
             case "password" -> {
-                String newPassword;
-                while (true) {
-                    newPassword = console.getStringInput("Please enter your password (min 8 chars, 1 uppercase, 1 lowercase, 1 number):");
-                    if(userService.validatePassword(newPassword)) {
-                        break;
-                    }
-                    console.error("Invalid password format. Please ensure it meets the requirements.");
-                }
+                String newPassword = console.getValidPasswordInput();
                 userToEdit.setPassword(PasswordService.hash(newPassword));
             }
 
             case "dateOfBirth" -> {
-                String newDateOfBirth;
-                while (true) {
-                    newDateOfBirth = console.getStringInput("Please enter your date of birth (DD-MM-YYYY): ");
-                    if (userService.validateDateOfBirth(newDateOfBirth)) {
-                        break;
-                    }
-                    console.error("Invalid date of birth format. Ensure the format is correct (e.g., 15-08-2010) and that you are at least 13 years old.");
-                }
+                String newDateOfBirth = console.getValidDateOfBirthInput();
                 userToEdit.setDateOfBirth(LocalDate.parse(newDateOfBirth, formatter));
             }
 
--- App/CLI-backend/src/main/java/cli/backend/commands/startmenu/RegisterCommand.java
+++ App/CLI-backend/src/main/java/cli/backend/commands/startmenu/RegisterCommand.java
@@ -2,8 +2,6 @@ package cli.backend.commands.startmenu;
 
 
 import cli.backend.commands.Command;
-import cli.backend.duplicates.CheckDuplicate;
-import cli.backend.duplicates.UserDuplicate;
 import cli.backend.userinterface.readers.Console;
 import cli.backend.services.PasswordService;
 import cli.backend.services.UserService;
@@ -15,59 +13,16 @@ public class RegisterCommand implements Command {
     public boolean execute() {
         Console console = Console.getInstance();
         UserService userService = UserService.getInstance();
-        CheckDuplicate userCheck=new UserDuplicate();
 
         console.info("Welcome to the registration page.");
 
-        String username;
-        while(true) {
+        String username = console.getValidUsernameInput();
 
-            username = console.getStringInput("Please enter your username (4-20 characters, alphanumeric):");
+        String email = console.getValidEmailInput();
 
-            if (!userService.validateUsername(username)) {
-                console.error("Invalid username format. Please try again.");
-                continue;
-            }
+        String password = console.getValidPasswordInput();
 
-            if (userCheck.isDuplicate(username)) {
-                console.error("Username already exists. Please choose a different username.");
-                continue;
-            }
-            break;
-        }
-
-        String email;
-        while(true){
-            email = console.getStringInput("Please enter your email address:");
-            if(!userService.validateEmail(email)) {
-                console.error("Invalid email format. Must be like 'user@domain.com'.");
-                continue;
-            }
-            if(userCheck.isDuplicate(email)) {
-                console.error("Email already exists. Please use a different email address.");
-                continue;
-            }
-
-            break;
-        }
-
-        String password;
-        while (true) {
-            password = console.getStringInput("Please enter your password (min 8 chars, 1 uppercase, 1 lowercase, 1 number):");
-            if(userService.validatePassword(password)) {
-                break;
-            }
-            console.error("Invalid password format. Please ensure it meets the requirements.");
-        }
-
-        String dateOfBirth;
-        while (true) {
-            dateOfBirth = console.getStringInput("Please enter your date of birth (DD-MM-YYYY): ");
-            if (userService.validateDateOfBirth(dateOfBirth)) {
-                break;
-            }
-            console.error("Invalid date of birth format. Ensure the format is correct (e.g., 15-08-2010) and that you are at least 13 years old.");
-        }
+        String dateOfBirth = console.getValidDateOfBirthInput();
 
         password = PasswordService.hash(password);
         userService.addUser(username, email, password, dateOfBirth);
