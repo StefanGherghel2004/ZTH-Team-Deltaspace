6b23ff3575c44b62d2034b0c560993b7226d7573
MihaiStan <mihaiviorelstan@gmail.com>
2026-07-28 16:50:14 +0300

    Implemented register user command

---
+++ App/CLI-frontend/src/main/java/org/example/commands/UserValidator.java
@@ -0,0 +1,85 @@
+package org.example.commands;
+
+
+import org.example.User;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class UserValidator {
+
+    private static final int MIN_AGE = 13;
+
+    private static UserValidator instance;
+
+    private static final String USERNAME_REGEX = "^[a-zA-Z0-9._-]{4,20}$";
+    private static final String EMAIL_REGEX = "(?i)^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+
+    private UserValidator() {}
+
+    public static UserValidator getInstance() {
+        if (instance == null) instance = new UserValidator();
+        return instance;
+    }
+
+    public boolean validateUsername (String username) {
+
+        return (validate(username,USERNAME_REGEX) && !username.isEmpty());
+    }
+
+    public boolean validatePassword (String password) {
+
+        return (validate(password,PASSWORD_REGEX) && !password.isEmpty());
+    }
+
+    public boolean validateEmail (String email) {
+        return (validate(email,EMAIL_REGEX) && !email.isEmpty());
+    }
+
+    public boolean validateDateOfBirth (String dateOfBirth) {
+
+        if (dateOfBirth.isEmpty())
+            return false;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        try {
+
+            LocalDate birthday = LocalDate.parse(dateOfBirth, formatter);
+            LocalDate today = LocalDate.now();
+            Period age = Period.between(birthday, today);
+            return age.getYears() >= MIN_AGE;
+        } catch (DateTimeParseException e) {
+            System.out.println("Invalid date format!");
+            return false;
+        }
+    }
+
+//    public  User validateUserAccount (String usernameOrEmail, String password)
+//            throws InvalidUserAccountException, DeletedAccountException {
+//
+//        Optional<User> userOptional = userRepository.findByUsernameOrEmail(usernameOrEmail);
+//
+//        if (userOptional.isPresent()) {
+//            User user = userOptional.get();
+//
+//            if (user.isDeleted()) {
+//                throw new DeletedAccountException();
+//            }
+//
+//            if (PasswordService.verify(password, user.getPassword())) {
+//                return user;
+//            }
+//        }
+//
+//        throw new InvalidUserAccountException();
+//    }
+
+    public  boolean validate(String user, String regex) {
+        return user != null && Pattern.matches(regex, user);
+    }
+}
