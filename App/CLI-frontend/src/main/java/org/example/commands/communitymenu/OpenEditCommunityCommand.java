0cc4765244b8cc07d61ea6dd26737d9b4c2a6c0c
SrbanM <serban2011@yahoo.com>
2026-07-28 17:12:12 +0300

    open edit community command

--- App/CLI-frontend/src/main/java/org/example/commands/communitymenu/OpenEditCommunityCommand.java
+++ App/CLI-frontend/src/main/java/org/example/commands/communitymenu/OpenEditCommunityCommand.java
@@ -2,12 +2,16 @@ package org.example.commands.communitymenu;
 
 
 import org.example.commands.Command;
+import org.example.handlers.AppHandler;
 
 public class OpenEditCommunityCommand implements Command {
 
 
     @Override
     public boolean execute() {
+
+        AppHandler appHandler = AppHandler.getInstance();
+        appHandler.setCurrentState(AppHandler.State.EDIT_COMMUNITY);
         return true;
     }
 
