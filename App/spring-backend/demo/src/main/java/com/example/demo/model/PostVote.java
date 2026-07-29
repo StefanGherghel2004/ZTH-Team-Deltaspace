b8d37e325d6ffc5600428e93e1ddeece7f61c51d
Stefan-Ciprian Gherghel <stefan.gherghel@stud.acs.upb.ro>
2026-07-29 12:27:44 +0300

    Now the posts are deleted when deleting community

--- App/spring-backend/demo/src/main/java/com/example/demo/model/PostVote.java
+++ App/spring-backend/demo/src/main/java/com/example/demo/model/PostVote.java
@@ -3,6 +3,8 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -14,6 +16,7 @@ public class PostVote extends BaseVote {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Post post;
 
 }
