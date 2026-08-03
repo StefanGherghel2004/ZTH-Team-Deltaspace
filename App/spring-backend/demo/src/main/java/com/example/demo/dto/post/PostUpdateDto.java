b6899edfc840cbbcf9a22d1c8a1ff1b6df3d21be
Stefan-Ciprian Gherghel <stefan.gherghel@stud.acs.upb.ro>
2026-08-03 20:12:18 +0300

    Add author field for Post Creation (not used)

--- App/spring-backend/demo/src/main/java/com/example/demo/dto/post/PostUpdateDto.java
+++ App/spring-backend/demo/src/main/java/com/example/demo/dto/post/PostUpdateDto.java
@@ -1,6 +1,7 @@
 package com.example.demo.dto.post;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 public class PostUpdateDto {
     @Size(min = 3, max = 300, message = "Title must be between 3 and 300 characters")
+    @Pattern(regexp = ".*\\S.*", message = "Title cannot consist only of spaces")
     private String title;
 
     @Size(max = 10000, message = "Content cannot exceed 10000 characters")
