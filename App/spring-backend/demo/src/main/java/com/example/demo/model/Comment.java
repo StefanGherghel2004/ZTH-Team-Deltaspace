f44544db968fd3d3229d01d4b325b0b0e60f0425
SrbanM <serban2011@yahoo.com>
2026-08-11 17:19:19 +0300

    remove subreddit/comment builder beacuse it did not work

--- App/spring-backend/demo/src/main/java/com/example/demo/model/Comment.java
+++ App/spring-backend/demo/src/main/java/com/example/demo/model/Comment.java
@@ -3,24 +3,18 @@ package com.example.demo.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Formula;
 
-@Builder
+
 @Data
 @Entity
 @Table(name = "comments")
-@AllArgsConstructor
-@NoArgsConstructor
 public class Comment extends BaseEntity {
 
     @Column(length = 1000, unique = false,nullable = false)
     private String content;
 
-    @Builder.Default
     @Column(name = "deleted", unique = false, nullable = false)
     private boolean deleted = false;
 
@@ -44,11 +38,9 @@ public class Comment extends BaseEntity {
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "replies", "parentComment"})
     private Comment parentComment;
 
-    @Builder.Default
     @Column(nullable = false)
     private int upvotes = 0;
 
-    @Builder.Default
     @Column(nullable = false)
     private int downvotes = 0;
 
